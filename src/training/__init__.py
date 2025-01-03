2340540909aff1c8a9c73af8fc28a425a81cdbb3
datavoid <izquierdoxander@gmail.com>
2025-01-03 10:50:02 -0500

    refactor: Simplify training module imports and exports

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,34 +1,9 @@
-from src.training.trainers.sdxl_trainer import SDXLTrainer, save_checkpoint
-from src.training.trainers.base_router import BaseRouter
+from src.training.trainers.sdxl_trainer import SDXLTrainer
 from src.training.trainers.methods.ddpm_trainer import DDPMTrainer
 from src.training.trainers.methods.flow_matching_trainer import FlowMatchingTrainer
-from src.training.optimizers import AdamWBF16, AdamWScheduleFreeKahan, SOAP
-from src.training.schedulers import (
-    configure_noise_scheduler,
-    get_karras_sigmas,
-    get_sigmas,
-    get_scheduler_parameters,
-    get_add_time_ids
-)
 
 __all__ = [
-    # Trainer implementations
-    "SDXLTrainer",
-    "save_checkpoint",
-    # Training methods
-    "BaseRouter",
-    "DDPMTrainer", 
-    "FlowMatchingTrainer",
-    
-    # Optimizers
-    "AdamWBF16",
-    "AdamWScheduleFreeKahan", 
-    "SOAP",
-    
-    # Scheduler utilities
-    "configure_noise_scheduler",
-    "get_karras_sigmas",
-    "get_sigmas", 
-    "get_scheduler_parameters",
-    "get_add_time_ids"
+    'SDXLTrainer',
+    'DDPMTrainer',
+    'FlowMatchingTrainer'
 ]
